70f5b554a34341802fa59739acb978bda67ed5d1
Dom Rodriguez <shymega@shymega.org.uk>
2020-07-26 00:03:37 +0100

    Initial commit

---
+++ src/main/java/com/bookports/bookportscore/BookportsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookportsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookportsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookports/bookportscore/BookportsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookportsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
